8305d7cd2200ef7af2ecdbcb528bbb705afc2df7
Faraz Hanif <hfaraz745@gmail.com>
2018-04-17 22:48:38 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(num)
+  number = num
+  while number > 0 
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"  
 end
